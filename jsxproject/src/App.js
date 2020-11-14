67486628ace6596b905f79982928ad43dcfca6fc
Ghassen <tekayaghassen@hotmail.fr>
2020-11-14 12:57:41 +0100

    adding jsx

---
+++ jsxproject/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border:"solid 1px black"}}>
+
+        <h1 className="title red">Ben tekaya Ghassen</h1>
+
+        <br />
+
+<img src="/pic1.png" width="20%"/>
+
+<br />
+
+<img src="/pic2.png" width="20%"/>
+</div>
+<video width="320" height="240" controls>
+
+ <source src="(64) El Castro - Mouch 9olto ft A.L.A & Kaso - YouTube - Google Chrome 2020-03-17 14-54-21.mp4" type="video/mp4" />
+
+</video>
+</div>
+
+  );
+}
+
+export default App;
